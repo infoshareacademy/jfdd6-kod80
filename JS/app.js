d9c6ff9d58d625ec5d8e10df5936c53b2e75cb43
Karolina Kreft <kar.kreft@gmail.com>
2017-02-16 16:34:45 +0100

    Uploading custombox for the popup widnow. Changed background funkcjonalności.

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by karolinakreft on 16.02.17.
+ */
+
+// Instantiate new modal
+var modal = new Custombox.modal({
+  content: {
+    effect: 'fadein',
+    target: '#modal'
+  }
+});
+
+// Open
+modal.open();
