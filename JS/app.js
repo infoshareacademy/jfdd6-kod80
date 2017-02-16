581d7061aa89e65f3e918aff085748793564db00
Karolina Kreft <kar.kreft@gmail.com>
2017-02-16 16:45:00 +0100

    Deleted custombox for popup window - decided to use bootstrap instead

--- JS/app.js
+++ JS/app.js
@@ -2,13 +2,3 @@
  * Created by karolinakreft on 16.02.17.
  */
 
-// Instantiate new modal
-var modal = new Custombox.modal({
-  content: {
-    effect: 'fadein',
-    target: '#modal'
-  }
-});
-
-// Open
-modal.open();
