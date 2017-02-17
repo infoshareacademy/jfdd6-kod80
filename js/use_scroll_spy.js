2bf99770b6a9a7026d1bc8c9b1c0c9508ea0bb47
Paweł Szymański <peal1982@gmail.com>
2017-02-17 16:49:56 +0100

    feature/scrollSpyMenu Prosty działający ScrollSpy Nie działa jeszcze rewelacyjnie, ale jest to dobry wyjscie do zmian.

--- js/use_scroll_spy.js
+++ js/use_scroll_spy.js
@@ -1,12 +1,15 @@
 /**
  * Created by pawelszymanski on 17.02.17.
  */
-
+var dodajFocusStyle = (selector) => {
+  $(selector).focus();
+}
 $('.tu-bede-nawigowal').on('scrollSpy:enter', function() {
   console.log('enter:', $(this).attr('id'));
   var idOfVisibleElement = $(this).attr('id');
   const koncowka_id_nav = '_nav';
-  $('#' + idOfVisibleElement + koncowka_id_nav).css('color', 'red');
+  var selector = "#" + idOfVisibleElement + koncowka_id_nav;
+  dodajFocusStyle(selector);
 });
 
 $('.tu-bede-nawigowal').on('scrollSpy:exit', function() {
