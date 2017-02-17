7d2de86896d1f1b9065acc582da65d265426215d
Paweł Szymański <peal1982@gmail.com>
2017-02-17 16:35:12 +0100

    feature/scrollSpyMenu zmien kolor nawigacji gdy pojawi sie element na stronie

--- js/use_scroll_spy.js
+++ js/use_scroll_spy.js
@@ -1,8 +1,12 @@
 /**
  * Created by pawelszymanski on 17.02.17.
  */
+
 $('.tu-bede-nawigowal').on('scrollSpy:enter', function() {
   console.log('enter:', $(this).attr('id'));
+  var idOfVisibleElement = $(this).attr('id');
+  const koncowka_id_nav = '_nav';
+  $('#' + idOfVisibleElement + koncowka_id_nav).css('color', 'red');
 });
 
 $('.tu-bede-nawigowal').on('scrollSpy:exit', function() {
