eafbb5716f700d2dd5afd14a6d1a90c3f88b88ad
Karolina Kreft <kar.kreft@gmail.com>
2017-02-18 21:53:50 +0100

    Small changes on popup2 window.

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,20 @@
-/**
- * Created by karolinakreft on 16.02.17.
- */
+// /**
+//  * Created by karolinakreft on 16.02.17.
+//  */
 
+function ValidationEvent() {
+// Storing Field Values In Variables
+    var email = document.getElementById("podajMaila").value;
+// Regular Expression For Email
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+// Conditions
+    if (!document.getElementById("zgoda1").checked) {
+        alert("1");
+    } else if (!document.getElementById("zgoda2").checked) {
+        alert("2");
+    } else if (email == '' || !email.match(emailReg)) {
+        alert("3");
+    } else {
+        window.location.href = "#popup2";
+    }
+}
