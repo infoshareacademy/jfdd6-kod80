c73c6963c2d258eecde4b5fefe18d62eb286cdad
PawelSzy <peal1982@gmail.com>
2017-02-18 18:10:46 +0100

    feature/scrollSpyMenu znajdz top element

--- js/use_scroll_spy.js
+++ js/use_scroll_spy.js
@@ -7,19 +7,42 @@ var nav_Ids = (selector) => {
 	return $(".tu-bede-nawigowal").toArray().map( (element) => { return $(element).attr("id") } )
 };
 
+var visibleElements = new Set();
+
 var dodajFocusStyle = (selector) => {
   $(selector).focus();
 }
+
+var createSelector = (id) => {
+	 const koncowka_id_nav = '_nav';
+	 return "#" + id + koncowka_id_nav;
+}
+
+var sorted_visibleElements = () => { return nav_Ids().filter( e => visibleElements.has(e) )};
+
+var top_element_id= () => {
+	return 	sorted_visibleElements()[0];
+}
+
+
 $('.tu-bede-nawigowal').on('scrollSpy:enter', function() {
   console.log('enter:', $(this).attr('id'));
+
   var idOfVisibleElement = $(this).attr('id');
-  const koncowka_id_nav = '_nav';
-  var selector = "#" + idOfVisibleElement + koncowka_id_nav;
+  var selector = createSelector(idOfVisibleElement);
+
   dodajFocusStyle(selector);
+  visibleElements.add(idOfVisibleElement);
 });
 
 $('.tu-bede-nawigowal').on('scrollSpy:exit', function() {
   console.log('exit:', $(this).attr('id'));
+
+  var idOfVisibleElement = $(this).attr('id');
+  var selector = createSelector(idOfVisibleElement);
+	visibleElements.delete(idOfVisibleElement);
+
+
 });
 
 $('.tu-bede-nawigowal').scrollSpy();
