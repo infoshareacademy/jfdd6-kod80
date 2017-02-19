4a571625c6f27ad8ee257bb8e24a41dc9e86d596
Karolina Kreft <kar.kreft@gmail.com>
2017-02-19 12:13:21 +0100

    Centered the agreement checkbox, changed content

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,20 @@
  * Created by karolinakreft on 16.02.17.
  */
 
+<<<<<<< Updated upstream
+=======
+function ValidationEvent() {
+// Storing Field Values In Variables
+    var email = document.getElementById("podajMaila").value;
+// Regular Expression For Email
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+// Conditions
+    if (!document.getElementById("zgoda1").checked) {
+        alert("Nie możemy poinformować Cie o premierze drogą elektroniczną, bez zaznaczenia na to zgody :(");
+    } else if (email == '' || !email.match(emailReg)) {
+        alert("Ups! Chyba wpisałeś niepoprawny adres e-mail.");
+    } else {
+        window.location.href = "#popup2";
+    }
+}
+>>>>>>> Stashed changes
