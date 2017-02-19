72b5f1a6597a589d56a8df1668c16c52ba7b6ce2
PawelSzy <peal1982@gmail.com>
2017-02-19 12:14:19 +0100

    feature/scrollSpyMenu  Code Refactor Popraw wyglad kodu, dodaj komentarze

--- js/use_scroll_spy.js
+++ js/use_scroll_spy.js
@@ -2,6 +2,7 @@
  * Created by pawelszymanski on 17.02.17.
  */
 	
+//tablica wszystkich elementow nawigacji  
 var nav_Ids = (selector) => {
 	return $(".tu-bede-nawigowal").toArray().map( (element) => { return $(element).attr("id") } )
 };
@@ -12,6 +13,9 @@ var dodajFocusStyle = (selector) => {
   $(selector).focus();
 }
 
+//utworz selector
+// @param - id
+// @return string #id_nav
 var createSelector = (id) => {
 	 const koncowka_id_nav = '_nav';
 	 return "#" + id + koncowka_id_nav;
@@ -23,14 +27,27 @@ var top_element_id= () => {
 	return 	sorted_visibleElements()[0];
 }
 
-var chose_focus_middle_element_id = () => {
+var middle_element_id = () => {
 	return sorted_visibleElements()[Math.floor(sorted_visibleElements().length / 2)];
+}
+
+var bottom_element_id = () => {
+  let ids = nav_Ids();
+  return ids[ids.length - 1] ;
 }
 
 var focus_element = (id) => {
 	const selector = createSelector(id);
 	dodajFocusStyle(selector);
 }
+
+
+/*„DOBRZY ARTYSCI KOPIUJA, WIELCY KRADNA.” PICASSO
+* Element wziety dzieki uprzejmosci: "John Smart", 
+*Wykrywamy kiedy element pojawia sie na ekranie i kiedy znika
+*Jeśli jest widoczny dodaj do zbioru elementów widocznch, jeśli znika usuń
+*
+*/
 
 $('.tu-bede-nawigowal').on('scrollSpy:enter', function() {
   console.log('enter:', $(this).attr('id'));
@@ -39,7 +56,7 @@ $('.tu-bede-nawigowal').on('scrollSpy:enter', function() {
   var selector = createSelector(idOfVisibleElement);
   visibleElements.add(idOfVisibleElement);
 
-  focus_element( chose_focus_middle_element_id() );
+  focus_element( middle_element_id() );
 });
 
 $('.tu-bede-nawigowal').on('scrollSpy:exit', function() {
@@ -50,24 +67,38 @@ $('.tu-bede-nawigowal').on('scrollSpy:exit', function() {
   visibleElements.delete(idOfVisibleElement);
 });
 
-$('.tu-bede-nawigowal').scrollSpy();
 
+//****************************************//
+
+
+//Sprawdz czy scrollSpy zostal uruchomionu
+let isSpying = false;
+var startSpying = () => {
+  if (isSpying == false) {
+    $('.tu-bede-nawigowal').scrollSpy();
+    isSpying = true;  }
+};
+
+
+//Uruchom scroll Spy przy pierwszym ruchy scrolla
+//
 $(document).scroll( () => {
- focus_element( chose_focus_middle_element_id() );
-} );
+  startSpying();
 
+//wybierz element do focus
+ focus_element( middle_element_id() );
 
-$(window).scroll(function() {
-   if($(window).scrollTop()  == 0) {
- 		focus_element( top_element_id() );
+ //rozpoznaj czy jestesm na gorze ekranu i zaznacz pierwszy element nawigacji
+  if($(window).scrollTop()  == 0) {
+    focus_element( top_element_id() );
    }
-});
 
-$(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+//rozpoznaj czy jestesm na dole ekranu i zaznacz dolny element nawigacji
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
        let ids = nav_Ids();
-       focus_element(ids[ids.length - 1]);
+       focus_element( bottom_element_id() );
    }
 });
 
+//Focus pierwszy, gorny element - wyswietlanie przy pierwszym wyswietlaniu obrazu
 focus_element(nav_Ids()[0]);
