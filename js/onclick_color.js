c5c13a2c9421a6632e0c4f645f0851eb0252c3eb
Agata Cała <agata.cala@gmail.com>
2017-02-21 16:52:12 +0100

    feature/FD6K8-26 setInterval function

--- js/onclick_color.js
+++ js/onclick_color.js
@@ -1,3 +1,14 @@
-/**
- * Created by agatacala on 21.02.17.
- */
+setInterval(function () {
+  document.querySelectorAll('.set li').forEach(function (element) {
+    element.classList.remove('correct-key');
+  });
+
+  document.querySelectorAll('.set li')[Math.floor(Math.random() * 12)].classList.add('correct-key');
+}, 1000);
+
+document.querySelectorAll('.set li').forEach(function (element) {
+  console.log(element);
+  element.addEventListener('click', function () {
+    element.classList.add('onclick-color-white');
+  })
+});
