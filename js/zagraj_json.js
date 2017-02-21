66d1c51a437324c5a240797aecd73157bfe69d2c
Paweł Szymański <peal1982@gmail.com>
2017-02-21 16:23:53 +0100

    feature/FD6K8-15/read-music_json Show all  tracks

--- js/zagraj_json.js
+++ js/zagraj_json.js
@@ -13,11 +13,33 @@ var zagraj = (nazwa_nuty, time, duration) => {
   console.log(time);
   console.log(duration);
 }
+var playTrack = (track) => {
 
-castles_music.tracks[1].notes.forEach( (note) => {
-  const nazwa_nuty = note.name;
-  const time = note.time;
-  const duration = note.duration;
+  if (track.notes !== void 0) {
+    track.notes.forEach( (note) => {
+    const nazwa_nuty = note.name;
+    const time = note.time;
+    const duration = note.duration;
 
-  zagraj(nazwa_nuty, time, duration);
+    zagraj(nazwa_nuty, time, duration);
+    });
+  }
+
+
+  // track.forEach( (note) => {
+
+  // });
+};
+
+
+castles_music.tracks.forEach( (track) => {
+  // console.log(track);
+  playTrack( track );
+
 });
+
+
+
+// castles_music.tracks[1].notes.forEach( (note) => {
+//
+// });
