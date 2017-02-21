72c018f8b3f533e4610ee6314edcc900b33744a1
Paweł Szymański <peal1982@gmail.com>
2017-02-21 17:01:53 +0100

    feature/FD6K8-15/read-music_json Pokaż w odpowiednim czasie nute i czas jej grania.

--- js/zagraj_json.js
+++ js/zagraj_json.js
@@ -4,14 +4,31 @@
 
 var music = castles_music;
 
-console.log(music);
+// console.log(music);
+//
+// console.log( castles_music.tracks[1].notes );
 
-console.log( castles_music.tracks[1].notes );
+
 
 var zagraj = (nazwa_nuty, time, duration) => {
-  console.log(nazwa_nuty);
-  console.log(time);
-  console.log(duration);
+  // console.log(nazwa_nuty);
+  // console.log(time);
+  // console.log(duration);
+
+  function Nuta(nazwa_nuty, time, duration) {
+      this.nazwa_nuty = nazwa_nuty;
+      this.time = time;
+      this.duration = duration;
+
+      this.play = function () {
+        console.log(this.nazwa_nuty, this.duration);
+      };
+  }
+  var nuta = new Nuta(nazwa_nuty, time, duration);
+
+  var playNoteTimeoutID = setTimeout(function() {
+    nuta.play();
+  }, time*1000);
 }
 
 var playTrack = (track) => {
