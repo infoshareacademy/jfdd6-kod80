643705018fe9ed010a8cd2385d18ffb9e848ec94
Marek Kita <kitamarekweb@gmail.com>
2017-02-22 16:55:21 +0100

    FD6K8-26 Mati helped with playing music

--- js/onclick_color.js
+++ js/onclick_color.js
@@ -1,14 +1,25 @@
-setInterval(function () {
-  document.querySelectorAll('.set li').forEach(function (element) {
-    element.classList.remove('correct-key');
-  });
+function playNote(note) {
+  var audio = new Audio('keyboard/'+note+'.mp3');
+  audio.play();
+}
 
-  document.querySelectorAll('.set li')[Math.floor(Math.random() * 12)].classList.add('correct-key');
-}, 1000);
-
-document.querySelectorAll('.set li').forEach(function (element) {
-  console.log(element);
-  element.addEventListener('click', function () {
-    element.classList.add('onclick-color-white');
-  })
+$('ul.set li').on('click',function () {
+  playNote($(this).attr('data-note'));
 });
+
+
+// bartek!!!!!!!!!!!!!!!!!!!!
+// setInterval(function () {
+//   document.querySelectorAll('.set li').forEach(function (element) {
+//     element.classList.remove('correct-key');
+//   });
+//
+//   document.querySelectorAll('.set li')[Math.floor(Math.random() * 12)].classList.add('correct-key');
+// }, 1000);
+//
+// document.querySelectorAll('.set li').forEach(function (element) {
+//   console.log(element);
+//   element.addEventListener('click', function () {
+//     element.classList.add('onclick-color-white');
+//   })
+// });
