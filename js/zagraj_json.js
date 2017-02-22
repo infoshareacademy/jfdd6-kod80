50e65e2248de547024ded6022bb928fb7a6cc4d1
Paweł Szymański <peal1982@gmail.com>
2017-02-22 09:41:33 +0100

    feature/FD6K8-15/read-music_json  Callback version - play note function.

--- js/zagraj_json.js
+++ js/zagraj_json.js
@@ -4,49 +4,47 @@
 
 var music = castles_music;
 
-// console.log(music);
-//
-// console.log( castles_music.tracks[1].notes );
+var show_note_in_console = (nazwa_nuty, duration) => {
+  console.log(nazwa_nuty, duration);
+}
 
+var zagraj = (nazwa_nuty, time, duration, callback_play_note) => {
 
-
-var zagraj = (nazwa_nuty, time, duration) => {
-  // console.log(nazwa_nuty);
-  // console.log(time);
-  // console.log(duration);
-
-  function Nuta(nazwa_nuty, time, duration) {
+  function Nuta(nazwa_nuty, time, duration, callback_play_note) {
       this.nazwa_nuty = nazwa_nuty;
       this.time = time;
       this.duration = duration;
 
       this.play = function () {
-        console.log(this.nazwa_nuty, this.duration);
+        callback_play_note(this.nazwa_nuty, this.duration);
       };
   }
-  var nuta = new Nuta(nazwa_nuty, time, duration);
+
+  var nuta = new Nuta(nazwa_nuty, time, duration, callback_play_note);
 
   var playNoteTimeoutID = setTimeout(function() {
     nuta.play();
   }, time*1000);
 }
 
-var playTrack = (track) => {
+var playTrack = (track, callback_play_note) => {
   if (track.notes !== void 0) {
     track.notes.forEach( (note) => {
     const nazwa_nuty = note.name;
     const time = note.time;
     const duration = note.duration;
 
-    zagraj(nazwa_nuty, time, duration);
+    zagraj(nazwa_nuty, time, duration, callback_play_note);
     });
   }
 };
 
-var playMusic = (music) => {
+var playMusic = (music, callback_play_note) => {
   music.tracks.forEach((track) => {
-    playTrack(track);
+    playTrack(track, callback_play_note);
   });
 };
 
-playMusic(castles_music);
+// Graj muzyko - uzyj odpowiedniej funkcji do grania nuty
+// @param - plik JSON z muzyka, callback grajacy nute
+playMusic(castles_music, show_note_in_console);
