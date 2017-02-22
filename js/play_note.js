370f2845728efc938a399942ccc47a03cd1e8d63
Paweł Szymański <peal1982@gmail.com>
2017-02-22 16:45:00 +0100

    feature/FD6K6-15/read_music_json zmiany

--- js/play_note.js
+++ js/play_note.js
@@ -2,29 +2,38 @@
  * Created by pawelszymanski on 22.02.17.
  */
 
-function playNote(nuta) {
+// function playNote(nuta) {
+//
+//   switch(nuta) {
+//     case 'white-b':
+//       var audio = new Audio('white-b.mp3');
+//       break;
+//     case '"white-a'
+//       var audio = new Audio('white-a.mp3');
+//       break;
+//
+//       audio.play();
+//   }
+// }
 
-  switch(nuta) {
-    case 'white-b':
-      var audio = new Audio('white-b.mp3');
-      break;
-    case '"white-a'
-      var audio = new Audio('white-a.mp3');
-      break;
 
-      audio.play();
-  }
+function dodajKolor(note_id, duration ) {
+  setInterval(function () {
+    document.querySelectorAll('.set li').forEach(function (element) {
+      element.classList.remove('correct-key');
+    });
+
+    document.querySelectorAll('.set li')[Math.floor(Math.random() * 12)].classList.add('correct-key');
+  }, 1000);
+
+  document.querySelectorAll('.set li').forEach(function (element) {
+    console.log(element);
+    element.addEventListener('click', function () {
+      element.classList.add('onclick-color-white');
+    })
+  });
 }
 
-
-function dodajKolor(klawisz, ) {
-
-}
-
-
-.....onclik() {
-  nuta = $this.getId;
-  playNote(nuta);
-  dodajKolor()
-
+function grajNute(nazwa_nuty, duration) {
+  dodajKolor(nazwa_nuty, duration);
 }
