8c11e192ddca241664c0a0874d84f1a4575f5654
Paweł Szymański <peal1982@gmail.com>
2017-02-23 14:18:57 +0100

    feature/FD6K6-15/read_music_json playing notes witch duration

--- js/play_note.js
+++ js/play_note.js
@@ -2,9 +2,19 @@
  * Created by pawelszymanski on 22.02.17.
  */
 
-function playNote(note) {
+function playNote(note, duration) {
   var audio = new Audio('keyboard/'+note+'.mp3');
-  audio.play();
+  if (duration == void 0 || isNaN(duration)) {
+    audio.play();
+  } else {
+    audio.loop = true;
+    audio.play();
+    const czasGrania = duration * 1000;
+
+    setTimeout(function () {
+        audio.Stop();
+    }, czasGrania);
+  }
 }
 
 $('ul.set li').on('click',function () {
@@ -12,25 +22,7 @@ $('ul.set li').on('click',function () {
 });
 
 function dodajKolor(note_id, duration ) {
-  // setInterval(function () {
-  //   document.querySelectorAll('.set li').forEach(function (element) {
-  //     element.classList.remove('correct-key');
-  //   });
-  //
-  //   document.querySelectorAll('.set li')[Math.floor(Math.random() * 12)].classList.add('correct-key');
-  // }, 1000);
-  //
-  // document.querySelectorAll('.set li').forEach(function (element) {
-  //   console.log(element);
-  //   element.addEventListener('click', function () {
-  //     element.classList.add('onclick-color-white');
-  //   })
-  // });
-
   const stala_czas_podswietlania = duration*1000;
-
-  console.log(stala_czas_podswietlania);
-
   var $element = $(" li[data-note="+note_id.toLowerCase()+"]");
 
   $element.addClass('correct-key');
@@ -41,9 +33,6 @@ function dodajKolor(note_id, duration ) {
 
 }
 
-// function () {
-//
-// }
 
 function grajNute(nazwa_nuty, duration) {
   dodajKolor(nazwa_nuty, duration);
