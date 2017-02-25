36cb15241402a3b4b29972e0e18e533089859981
Karolina Kreft <kar.kreft@gmail.com>
2017-02-25 21:49:48 +0100

    Dziala walidacja almost

--- js/app.js
+++ js/app.js
@@ -5,17 +5,47 @@
 // /**
 //  * Created by karolinakreft on 16.02.17.
 //  */
+
 function ValidationEvent() {
   // Storing Field Values In Variables
   var email = document.getElementById("podajMaila").value;
   // Regular Expression For Email
   var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
   // Conditions
+
   if (!document.getElementById("zgoda1").checked) {
-    alert("Zaznacza zapoznanie się ze zgodą dotyczącą przetwarzania adresu e-mail.");
+
+
+      showAlert();
+
+
   } else if (email == '' || !email.match(emailReg)) {
-    alert("Wprowadź poprawny adres e-mail.");
+      customAlert();
+
   } else {
     window.location.href = "#popup2";
   }
+}
+
+
+
+
+function alertCheckbox () {
+    var whitebg = document.getElementById("alert-background");
+    var alertBox = document.getElementById("alert-box");
+    whitebg.style.display = "none";
+    alertBox.style.display = "none";
+}
+function showAlert() {
+    var whitebg = document.getElementById("alert-background");
+    var alertBox = document.getElementById("alert-box");
+    whitebg.style.display = "block";
+    alertBox.style.display = "block";
+
+    var winWidth = window.innerWidth;
+    var winHeight = window.innerHeight;
+
+    alertBox.style.left = (winWidth/2) - 480/2 + "px";
+    alertBox.style.top = "150px";
+
 }
